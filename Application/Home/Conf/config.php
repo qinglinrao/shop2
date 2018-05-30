3d1e575469e3de8645d200513e0e6e23485c9edf
qinglinrao <827396327@qq.com>
2018-05-30 17:14:07 +0800

    详情页短链接访问。

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -1,6 +1,12 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+    //'URL_ROUTER_ON'   => false, //开启路由
+
+    //配置路由规则
+    /*'URL_MAP_RULES'=>array(
+        'product/:id'          => '/home/Index/index/id/:1',
+    ),*/
 
 	// 设置相关文件路径
 	'TMPL_PARSE_STRING' => array(
