5ff1490e08709cb50ad53a29abe06116089916e5
qinglinrao <827396327@qq.com>
2018-06-19 14:25:49 +0800

    关闭debug。

--- admin.php
+++ admin.php
@@ -4,7 +4,7 @@
 if(version_compare(PHP_VERSION,'5.3.0','<'))  die('运行环境中PHP版本不得低于5.3.0');
 
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为 False / True
-define('APP_DEBUG',True);
+define('APP_DEBUG',False);
 // 定义应用目录
 define('APP_PATH','./Application/');
 //  绑定模块
