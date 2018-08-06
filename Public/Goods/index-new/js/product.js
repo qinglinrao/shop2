aca2e63e452b00a421452fd1ad28520841050869
qinglinrao <827396327@qq.com>
2018-08-06 14:49:50 +0800

    邮编只能填写5位数。

--- Public/Goods/index-new/js/product.js
+++ Public/Goods/index-new/js/product.js
@@ -394,7 +394,7 @@
         var re= /^[1-9][0-9]{4}$/;
         if(!re.test(code)) {
             /!*alert('邮编格式错误');*!/
-            showMessage('The post/zip code is error');
+            showMessage('Please enter the five digit zip code ');
             $('#postcode').focus();
             return false;
         }
