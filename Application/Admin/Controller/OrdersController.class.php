010b1d029caac7d86530dd7be3661fb1c310b24d
qinglinrao <827396327@qq.com>
2018-08-15 19:08:41 +0800

    导出跨境一号物流1

--- Application/Admin/Controller/OrdersController.class.php
+++ Application/Admin/Controller/OrdersController.class.php
@@ -1496,6 +1496,221 @@ class OrdersController extends CommonController {
 
     }
 
+    # 一家新的物流（20180815）（顾总找到的跨境一号物流,可以在当地存储）
+    public function export_logistics_new(){
+        $db = M('orders');
+        $keyword = I('get.keyword') ? I('get.keyword') : '';
+        $keyword_num = I('get.keyword_num') ? I('get.keyword_num') : '';
+        if ($keyword) {
+            $where['g.goods_title'] = array('like','%' . $keyword . '%');
+        }
+
+        # 查询商品编号
+        if ($keyword_num) {
+            $where['g.goods_number'] = array('like','%' . $keyword_num . '%');
+        }
+
+        $tuanTime = date('Y-m-d H:i:s',time()-300);
+        $statue = I('get.statue') ? I('get.statue') : '';
+        if ($statue) {
+            /*if($statue == 10){*/
+            # 可能之前团购的状态是10，现在改成11，因为货到付款是10了。
+            if($statue == 11){
+                $where['o.statue'] = $statue;
+                # $where['o.create_at'] = array('gt',$tuanTime);
+            }else{
+                $where['o.statue'] = $statue;
+            }
+        }
+        $area = I('get.time_area');
+        if($area){
+            $times = explode('~',$area);
+            $start_at = $times[0];
+            $end_at = $times[1];
+            $where['o.create_at'] = array('between',"{$start_at},{$end_at}");
+        }
+        $admin_id = I('get.admin_id');
+        if($admin_id){
+            $where['o.admin_id'] = $admin_id;
+        }
+
+        $count 	= M('orders o')->join('pt_goods g on o.good_id=g.id')->where($where)->count();
+        $table 	= 'pt_orders o';
+        $join 	= array('LEFT JOIN pt_goods g on o.good_id=g.id');
+        $field 	= 'o.good_id,o.money, o.order_id,o.good_count,o.id,o.pw_info,o.wl_info,o.from,o.statue,o.create_at,o.user_id,g.goods_title,o.create_at,o.remark,g.admin_id,g.goods_number';
+        $order 	= 'o.id desc';
+        $list 	= M()->table($table)->join($join)->where($where)->field($field)->order($order)->select();
+
+        # 查询投放人名称
+        $order_ids = array();
+        $good_ids = array();
+        $admin_data = M('admin')->field('admin_id, admin_name')->select();
+        foreach($list as $k=>$v){
+            $userInfo = M('member')->field('phone,username,address,code')->find($v['user_id']);
+            $list[$k]['phone'] = $userInfo['phone'];
+            $list[$k]['username'] = $userInfo['username'];
+            $list[$k]['address'] = $userInfo['address'];
+            $list[$k]['code'] = $userInfo['code'];
+            //合并管理员名称
+            foreach($admin_data as $admin_val){
+                if($v['admin_id'] == $admin_val['admin_id']){
+                    $list[$k]['admin_name'] = $admin_val['admin_name'];
+                }
+            }
+
+
+        }
+
+        $list_new = array();
+        $good_array = array();
+        foreach($list as $v){
+            //获取订单id
+            $order_ids[] = $v['id'];
+            $good_ids[] = $v['good_id'];
+            $list_new[$v['id']] = $v;
+            # 收集商品id用于查询唯一sku
+            $good_array[$v['id']]['size_array'][] = $v['good_id'];
+        }
+        # 查询规格信息。
+        $where = array();
+        $where['order_id'] = array('in', $order_ids);
+        $size_data = M('orders_size')->field('order_id, color, size, weight')->where($where)->select();
+        if($size_data){
+            foreach ($size_data as $v){
+                # 合并规格信息
+                $list_new[$v['order_id']]['size_data'] = $v['color'] . ' ' . $v['size'] . ' ' . $v['weight'];
+
+                # 查询唯一sku
+                $where = array();
+                $where['good_id'] = $good_array[$v['order_id']]['size_array'][0];
+                if($v['color']) $where['color'] = $v['color'];
+                #if($v['size']) $where['size'] = $v['size'];
+                #if($v['weight']) $where['weight'] = $v['weight'];
+                $number_data = M('goods_size')->field("id, good_id, unique_sku")->where($where)->select();
+                if($number_data[0]['unique_sku']){
+                    $list_new[$v['order_id']]['goods_number'] = $number_data[0]['unique_sku'];
+                }
+                /*if($v['size']){
+                    $list_new[$v['order_id']]['goods_number'] .= 'T'.$v['size'];
+                }*/
+            }
+        }
+
+        # 查询采购额外信息。
+        $where = array();
+        $where['good_id'] = array('in', $good_ids);
+        $property_data = M('goods_property')->where($where)->select();
+        if($property_data){
+            foreach ($property_data as $val){
+                foreach ($list_new as $v){
+                    if($val['good_id'] == $v['good_id']){
+                        # 合并规格信息
+                        $list_new[$v['id']]['declared_pcs'] = $val['declared_pcs'];
+                        $list_new[$v['id']]['declared_value'] = $val['declared_value'];
+                        $list_new[$v['id']]['description_english'] = $val['description_english'];
+                        $list_new[$v['id']]['description_chinese'] = $val['description_chinese'];
+                        $list_new[$v['id']]['is_sensitive'] = $val['is_sensitive'];
+                        $list_new[$v['id']]['category'] = $val['category'];
+                    }
+
+                }
+            }
+        }
+
+        # 导出操作
+
+        ini_set('display_errors', 0);
+        ini_set('log_errors', 1);
+        error_reporting(E_ALL & ~E_NOTICE);
+
+        $writer = new \Org\Excel\xlsxwriter();
+
+        $filename = "物流DPE上传表格(二)".date("Ymd",time())."-".rand(100,999).".xlsx";
+        header('Content-disposition: attachment; filename="'.$writer::sanitize_filename($filename).'"');
+        header("Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+        header('Content-Transfer-Encoding: binary');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+
+        /*$rows = array(
+            array('2003','1','-50.5','2010-01-01 23:00:00','2012-12-31 23:00:00'),
+            array('2003','=B1', '23.5','2010-01-01 00:00:00','2012-12-31 00:00:00'),
+        );*/
+        $rows = array();
+        # 设置颜色#ffff00
+        $rowstyle = array();
+        /*$rows[] = array("S/No", "Consignee", "Cnee Add1", "Cnee Add2", "Cnee Country(马来西亚MY，新加坡SG)", "Cnee Postcode",
+            "Cnee ISO Code","Cnee Contact","Cnee Tel","Pcs", "Weight (gm)", "Description of Goods1 - English", "Size of Good1 - English",
+            "Description of Goods1 - Chinese", "Size of Good1 - Chinese", "Declared Currency","Declared Pcs", "Declared Value",
+            "Shipper - English(留空时为网站客户的对应信息）","Shipper - Chinese(留空时为网站客户的对应信息）", "Shipper Add1(留空时为网站客户的对应信息）",
+            "Shipper Add2(留空时为网站客户的对应信息）", "Shipper City(留空时为网站客户的对应信息）", "Shipper State(留空时为网站客户的对应信息）",
+            "Shipper Postcode(留空时为网站客户的对应信息）","Shipper ISO Code(留空时为网站客户的对应信息）",  "Shipper Contact(留空时为网站客户的对应信息）", "Shipper Email(留空时为网站客户的对应信息）",
+            "渠道(输入规则：PW for POSLAJU西马;PE for POSLAJU东马;S for SKYNET;A for ABX;G for GDEX;DL for 新加坡dragonlink;NV for 新加坡NINJIA;NM for 西马NINJIA;NM6 for 西马敏感NINJIA;CS for SKYNET-COD;SE for Soonest;CX for GDEX-COD;DHL for DHL)",
+            "COD(到付金额)", "属性（0-普货 1-敏感货)", "客户订单号", "货物分类(A-衣服 B-电子 C-鞋子 D-箱包 E-杂货)","产品编码（SKU）","订单留言");*/
+        $rows[] = array("Business Code","Logistics channel","Shipment Order ID","Consignee Name","Address Line 1","Address Line 2","Address Line 3",
+            "District","City","State","Destination Country Code", "Postal Code", "Phone Number", "Is COD", "Cash Delivery Value","Service1","Currency Code",
+            "Total Declared Value","Shipment Weight(kg)","Volume","Is Insured","Insurance","Sensitive","Shipping Service Code","Remarks",
+            "Chinese Name 1","English Name 1","SKU 1","h5_url 1","Quantity 1","Declared value 1",
+            "Chinese Name 2","English Name 2","SKU 2","h5_url 2","Quantity 2","Declared value 2",
+            "Chinese Name 3","English Name 3","SKU 3","h5_url 3","Quantity 3","Declared value 3",
+            "Chinese Name 4","English Name 4","SKU 4","h5_url 4","Quantity 4","Declared value 4",
+            "Chinese Name 5","English Name 5","SKU 5","h5_url 5","Quantity 5","Declared value 5",
+            );
+        $rows[] = array("商家编码","物流渠道","销售订单号","收件人姓名","收件人地址","","","收件人区/县","收件人城市","收件人省份","收件人国家二字简码",
+            "收件人邮编","收件人电话","是否COD","应收收款额","增值服务","MYR","货值","包裹重量","客户自测体积(CM*CM*CM*NUM)","是否保险",
+            "保额","是否有敏感货: Y/N","PDO","备注",
+            "报关商品名称","报关商品英文名称","商家商品编码","商品销售链接","包裹内商品数量","单价",
+            "","","","","","",
+            "","","","","","",
+            "","","","","","",
+            "","","","","","",);
+        foreach ($list_new as $key=>$val){
+            $val['goods_purchase_url'] = htmlspecialchars_decode(html_entity_decode($val['goods_purchase_url']));
+            # 去掉html标签
+            $val['goods_purchase_url'] = strip_tags($val['goods_purchase_url']);
+            # 件数根据用户购买数量来决定
+            $num = $val['declared_pcs'] ? ($val['declared_pcs']*$val['good_count']) : $val['good_count'];
+            /*$rows[] = array($val['id'],$val['username'],$val['address'],"","MY",$val['code'],"", "", $val['phone'],
+                "1", "0", $val['description_english'], $val['size_data'], $val['description_chinese'], "", "USD", $num, $val['declared_value'],
+                "Voling", "", "", "", "", "", "", "","", "", "NM", $val['money'], $val['is_sensitive'], $val['order_id']."\t",
+                $val['category'], $val['goods_number'], $val['remark']);*/
+
+            $is_sensitive = $val['is_sensitive'] == 1 ? "MY-DHL-COD-T" : "MY-DHL-COD-P";
+            $is_sensitive2 = $val['is_sensitive'] == 1 ? "Y" : "N";
+            $rows[] = array("A10118A",$is_sensitive,$val['order_id']."\t",$val['username'],$val['address'],"","","","todo","todo",
+                "MY",$val['code'],$val['phone'],"Y",$val['money'],"","MYR",$val['money'],1,"26*17*16*1","N","",$is_sensitive2,
+                "PDO","",
+                $val['goods_title'],$val['goods_title'],$val['goods_number'],"",$num,$val['declared_value'],
+                "","","","","","",
+                "","","","","","",
+                "","","","","","",
+                "","","","","","",);
+        }
+
+        # 染色--todo
+        /*$i = 1;
+        $j = 1;
+        $arr = array(1, 2, 3, 5, 6, 9, 10, 12, 13, 14, 15, 26, 27, 28, 29, 30);
+        foreach($rows as $row){
+            foreach ($row as $r)
+                if((in_array($j, $arr))){
+                    $rowstyle[] = array('fill'=>"#ffff00");
+                }else{
+                    $rowstyle[] = array('fill'=>"");
+                }
+                $j++;
+
+            $i++;
+        }*/
+        $writer->setAuthor('Some Author');
+        foreach($rows as $key=>$row)
+
+            $writer->writeSheetRow('Sheet1', $row, $rowstyle);
+        $writer->writeToStdOut();
+        exit(0);
+
+    }
+
     # 上传修改订单的excel数据
     public function upload_excel_EditStatus() {
         $type = 'file';
@@ -1613,6 +1828,56 @@ class OrdersController extends CommonController {
 
     # 根据excel表生成马来西亚的地区文件
     public function upload_excel_do_address() {
+        header('Content-Type:application/json; charset=utf-8');
+        set_time_limit(0);
+        ini_set("memory_limit", "1024M");
+        # ./Uploads/Orders_Excel/5b2cb0dace147.xls
+        $url = I('post.url');
+
+        # $content = file_get_contents($url);
+        # $content = mb_convert_encoding ( $content, 'UTF-8','Unicode');
+
+        $spreadsheet = IOFactory::load($url);
+        $sheetData = $spreadsheet->getActiveSheet()->toArray(null, true, true, true);
+        unset($sheetData[1]);
+        $data = array();
+        foreach ($sheetData as $key=>$val){
+            $data[$val['A']][strtolower(trim($val['B']))][strtolower(trim($val['D']))][$val['C']]['name'] = $val['C'];
+            $data[$val['A']][strtolower(trim($val['B']))][strtolower(trim($val['D']))][$val['C']]['code'] = $val['E'];
+        }
+
+
+        $data_json = Json_encode($data);
+        $data_json = "var address=".$data_json;
+
+        $filename="./Address.js";
+        file_put_contents($filename,$data_json);exit;
+
+        $db = M('orders');
+        foreach ($sheetData as $key=>$val){
+            if($key > 1){
+                $data = array();
+                # 处理订单状态
+                $data['statue'] = 9; //默认9是已完成
+                if((int)($val['A']) == 1){
+                    $data['statue'] = 11; // 11是拒签。
+                }
+                $where = array();
+                $where['order_id'] = $val['B'];
+                $db->where($where)->save($data);
+            }
+        }
+        if(file_exists($url)){
+            unlink($url);
+        }
+
+
+        $res = array('code'=>'ok','msg'=>'修改成功');
+        echo json_encode($res);
+    }
+
+    # 根据excel表生成马来西亚的地区文件
+    public function upload_excel_do_address2() {
         header('Content-Type:application/json; charset=utf-8');
         set_time_limit(0);
         ini_set("memory_limit", "1024M");
