b38c0d43f529410fa1847ce6d34385d5ec0d9f05
qinglinrao <827396327@qq.com>
2018-08-29 18:17:34 +0800

    新首页2

--- Public/Goods/index-product-list/js/index-product-list.js
+++ Public/Goods/index-product-list/js/index-product-list.js
@@ -1,1 +1,30 @@
-//alert(1122);
+$(function($) {
+    $("#left_menu").click(function(){
+        // $(".uU16SS").addClass("left_menu_show");
+        //$(".uU16SS").animate({left: '0px'}, "slow");
+        $(".uU16SS").css({
+            "transform": "translate3d(0px, 0px, 0px)",
+            "transition-duration": "500ms"
+        });
+        $("._3p1vfs").css({
+            "opacity": "1"
+        });
+    });
+
+
+    var navOffset=$("._354-78").offset().top;
+    $(window).scroll(function(){
+        var scrollPos=$(window).scrollTop();
+        if(scrollPos >=navOffset){
+            $("._354-78").css({
+                "transform": "translate3d(0px, 0px, 0px)",
+                "transition-duration": "500ms"
+            });
+        }else{
+            $("._354-78").css({
+                "transform": "translate3d(0px, -112px, 0px)",
+                "transition-duration": "500ms"
+            });
+        }
+    });
+});
