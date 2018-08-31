dbc63ade5c1a1aebffea259435248dafe324bd7e
qinglinrao <827396327@qq.com>
2018-08-31 16:25:24 +0800

    新首页4-隐藏左边栏

--- Public/Goods/index-product-list/js/index-product-list.js
+++ Public/Goods/index-product-list/js/index-product-list.js
@@ -8,11 +8,53 @@ $(function($) {
             "z-index": "999"
         });
         $("._3p1vfs").css({
-            "opacity": "1"
+            "opacity": "1",
+            "z-index": "1",
+            "pointer-events": "auto"
+        });
+        $("body").css({
+           "overflow-y": "hidden"
+        });
+        $("._1Mg3DU").css({
+            "pointer-events": "auto"
         });
 
 
     });
+
+    $("._3p1vfs").click(function () {
+        $(".uU16SS").css({
+            "transform": "translate3d(-102%, 0px, 0px)",
+            "transition-duration": "500ms",
+            "z-index": "1"
+        });
+        $("._3p1vfs").css({
+            "opacity": "0",
+            "z-index": "0",
+            "pointer-events": ""
+        });
+        $("body").css({
+            "overflow-y": "visible"
+        });
+        $("._1Mg3DU").css({
+            "pointer-events": ""
+        });
+
+    });
+    function left_menu_hide(){
+        alert('click222');
+        $(".uU16SS").css({
+            "transform": "translate3d(-102%, 0px, 0px)",
+            "transition-duration": "500ms",
+            "z-index": "1"
+        });
+        $("._3p1vfs").css({
+            "opacity": "0",
+            "z-index": "0"
+        });
+    };
+
+
 
 
     var navOffset=$("._354-78").height();
