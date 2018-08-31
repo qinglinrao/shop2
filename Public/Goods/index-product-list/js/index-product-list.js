463e4aa2d73ba1149d392312a376119654851040
qinglinrao <827396327@qq.com>
2018-08-31 10:24:56 +0800

    新首页3-先屏蔽掉跳转的a标签链接

--- Public/Goods/index-product-list/js/index-product-list.js
+++ Public/Goods/index-product-list/js/index-product-list.js
@@ -1,24 +1,28 @@
 $(function($) {
-    $("#left_menu").click(function(){
+    $(".left_menu").click(function(){
         // $(".uU16SS").addClass("left_menu_show");
         //$(".uU16SS").animate({left: '0px'}, "slow");
         $(".uU16SS").css({
             "transform": "translate3d(0px, 0px, 0px)",
-            "transition-duration": "500ms"
+            "transition-duration": "500ms",
+            "z-index": "999"
         });
         $("._3p1vfs").css({
             "opacity": "1"
         });
+
+
     });
 
 
-    var navOffset=$("._354-78").offset().top;
+    var navOffset=$("._354-78").height();
     $(window).scroll(function(){
         var scrollPos=$(window).scrollTop();
         if(scrollPos >=navOffset){
             $("._354-78").css({
                 "transform": "translate3d(0px, 0px, 0px)",
-                "transition-duration": "500ms"
+                "transition-duration": "500ms",
+                "z-index": "999"
             });
         }else{
             $("._354-78").css({
